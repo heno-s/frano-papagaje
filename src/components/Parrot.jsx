dba4225892e0c9c588e2e39cda57f494d8f7d6d7
janko12 <henrich.sury97@gmail.com>
2023-06-28 10:09:48 +0200

    Remove lazy loading
    When lazy loading is on, images on the iphone load slowly, until it is
    not scrolled in. It would be better to load all of them up front

--- src/components/Parrot.jsx
+++ src/components/Parrot.jsx
@@ -13,7 +13,6 @@ export default function Parrot({
                 <img
                     src={image}
                     alt={name}
-                    loading="lazy"
                     onClick={() => handleImageClick(image)}
                 />
                 <div>
